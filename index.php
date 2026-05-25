85ac7c929e64b6cb4ab241eb28d0cfdc69a62b62
s2301118 <s2301118@usls.edu.ph>
2026-05-25 19:40:34 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title>My Home Page</title>
+  </head>
+  <body>
+    <h1>Welcome to My Website! I am Seth!</h1>
+    <ul>
+      <li><a href="index.php">HOME</a></li>
+      <li><a href="/LT2/index.html">LEARNING TASK</a></li>
+    </ul>
+  </body>
+  
+</html>
